96187854a06736d1d07e37c23f61190f9ce92cea
Nicolas Lurkin <nlurkin@gmail.com>
2013-11-29 14:28:42 +0000

    Implementation of the Compute method

--- AttitudeLoop.cpp
+++ AttitudeLoop.cpp
@@ -16,3 +16,15 @@ AttitudeLoop::~AttitudeLoop() {
 	// TODO Auto-generated destructor stub
 }
 
+VectorFloat AttitudeLoop::Compute(Quaternion qM, VectorFloat omegaM) {
+	Quaternion qErr;
+	VectorFloat axisErr;
+
+	qErr = fQRef * qM;
+
+	if(qErr.w < 0) axisErr = -VectorFloat(qErr);
+	else axisErr = VectorFloat(qErr);
+
+	fTorque = -axisErr*fPQ - omegaM*fPOmega;
+	return fTorque;
+}
--- AttitudeLoop.h
+++ AttitudeLoop.h
@@ -8,10 +8,29 @@
 #ifndef ATTITUDELOOP_H_
 #define ATTITUDELOOP_H_
 
+#include <Arduino.h>
+#include "helper_3dmath.h"
+
 class AttitudeLoop {
 public:
 	AttitudeLoop();
 	virtual ~AttitudeLoop();
+
+	VectorFloat Compute(Quaternion qM, VectorFloat omegaM);
+
+	const Quaternion& getQRef() const {return fQRef;}
+	float getPq() const {return fPQ;}
+	float getPOmega() const {return fPOmega;}
+	void setQRef(const Quaternion& qRef) {fQRef = qRef;}
+	void setP(float pQ, float pOmega) {
+		fPQ = pQ;
+		fPOmega = pOmega;
+	}
+
+private:
+	Quaternion fQRef;
+	float fPQ, fPOmega;
+	VectorFloat fTorque;
 };
 
 #endif /* ATTITUDELOOP_H_ */
