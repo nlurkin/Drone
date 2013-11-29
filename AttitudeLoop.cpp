7618d2e68a1f8d982df347402fc1d8ddb79c4e8d
Nicolas Lurkin <nlurkin@gmail.com>
2013-11-29 13:49:37 +0000

    Adding AttitudeLoop class and TODO for PPQuaternion Loop

---
+++ AttitudeLoop.cpp
@@ -0,0 +1,18 @@
+/*
+ * AttitudeLoop.cpp
+ *
+ *  Created on: 29 nov. 2013
+ *      Author: Nicolas
+ */
+
+#include "AttitudeLoop.h"
+
+AttitudeLoop::AttitudeLoop() {
+	// TODO Auto-generated constructor stub
+
+}
+
+AttitudeLoop::~AttitudeLoop() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ AttitudeLoop.h
@@ -0,0 +1,17 @@
+/*
+ * AttitudeLoop.h
+ *
+ *  Created on: 29 nov. 2013
+ *      Author: Nicolas
+ */
+
+#ifndef ATTITUDELOOP_H_
+#define ATTITUDELOOP_H_
+
+class AttitudeLoop {
+public:
+	AttitudeLoop();
+	virtual ~AttitudeLoop();
+};
+
+#endif /* ATTITUDELOOP_H_ */
