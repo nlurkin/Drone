5427e40a4130ff5fa405b0ddeea64f6f0dfa66f0
Nicolas <Nicolas@Nicolas-surface>
2014-01-05 21:30:53 +0000

    Determination of I and R more or less correct. Weird thing appearing when trying to determine it for a long time

--- Simulation/DroneSimulator/Body.py
+++ Simulation/DroneSimulator/Body.py
@@ -151,12 +151,12 @@ class Body(object):
         self.computeQuaternion(dt)
         
         #print "oldomega " + oldomega
-        print "oldomega " + ooldomega
-        print "self.oldomega + " + self.oldomega
-        I = (oldaplha[0]-self.Alpha[0])/(ooldomega[1]*ooldomega[2]-self.Omega[1]*self.Omega[2])
-        print "I Body " + str(I)
-        I = (self.I[2]-self.I[1])/self.I[0]
-        rx = self.L*self.m1.K*pow(Params.MaxOmega,2)/(10000.*self.I[0])
+        #print "oldomega " + ooldomega
+        #print "self.oldomega + " + self.oldomega
+        #I = (oldaplha[0]-self.Alpha[0])/(ooldomega[1]*ooldomega[2]-self.Omega[1]*self.Omega[2])
+        #print "I Body " + str(I)
+        #I = (self.I[2]-self.I[1])/self.I[0]
+        #rx = self.L*self.m1.K*pow(Params.MaxOmega,2)/(10000.*self.I[0])
         '''print "true I " + str(I)
         print "true rx " + str(rx)
         print "true alpha " + str(self.Alpha)
@@ -165,11 +165,13 @@ class Body(object):
         print "torque " + str(self.Torque[0]/self.I[0])
         print "torque ici " + str(rx*pow(self.m1.Power,2))
         print "alpha second terme " + str(oldomega[1]*oldomega[2]*I)'''
-        alpha = pow(self.m1.Power,2)*rx - ooldomega[1]*ooldomega[2]*I
+        #alpha = pow(self.m1.Power,2)*rx - ooldomega[1]*ooldomega[2]*I
+        I = -0.887005649718
         Rx = (self.Alpha[0]+ooldomega[1]*ooldomega[2]*I)/pow(self.m1.Power,2)
+        #print Rx
         #print "calxulated alpha " + str(alpha)
-        print "Calculated rx " + str(Rx)
-        self.oldomega = Vector(ooldomega)
+        #print "Calculated rx " + str(Rx)
+        #self.oldomega = Vector(ooldomega)
         
         
     def setMotorConstants(self, Rho, K_v, K_t, K_tau, I_M, A_swept, A_xsec, Radius, C_D):
@@ -186,7 +188,6 @@ class Body(object):
     
     def computeMotor(self, dt):
         if not self.TorqueIsSet:
-            print self.CtrlInput
             self.m1.setMeasure(self.CtrlInput[0], dt)
             self.m2.setMeasure(self.CtrlInput[1], dt)
             self.m3.setMeasure(self.CtrlInput[2], dt)
@@ -195,7 +196,7 @@ class Body(object):
     def computeTorque(self):
         if not self.TorqueIsSet:
             self.Torque = Vector([self.L*(self.m1.Thrust-self.m3.Thrust), self.L*(self.m2.Thrust-self.m4.Thrust), self.m1.Tau_z+self.m2.Tau_z+self.m3.Tau_z+self.m4.Tau_z])
-            print "M1 thrust " + str(self.L*self.m1.Thrust)
+            '''print "M1 thrust " + str(self.L*self.m1.Thrust)
             print "M2 thrust " + str(self.L*self.m2.Thrust)
             print "M3 thrust " + str(self.L*self.m3.Thrust)
             print "M4 thrust " + str(self.L*self.m4.Thrust)
@@ -203,7 +204,7 @@ class Body(object):
             print "M2 tauZ " + str(self.m2.Tau_z)
             print "M3 tauZ " + str(self.m3.Tau_z)
             print "M4 tauZ " + str(self.m4.Tau_z)
-            print "Body torque " + self.Torque
+            print "Body torque " + self.Torque'''
         else:
             self.Torque = self.CtrlInput
         
@@ -212,11 +213,11 @@ class Body(object):
     
     def computeAlpha(self, dt):
         #print "Omega alpha: " + self.Omega
-        oldalpha = self.Alpha
+        #oldalpha = self.Alpha
         self.Alpha = Vector([(self.Torque[0] - (self.I[1]-self.I[2])*self.Omega[1]*self.Omega[2])/self.I[0],
                       (self.Torque[1] - (self.I[2]-self.I[0])*self.Omega[0]*self.Omega[2])/self.I[1],
                       (self.Torque[2] - (self.I[0]-self.I[1])*self.Omega[0]*self.Omega[1])/self.I[2]])
-        print "I factor apha " + str((self.Alpha[0] - oldalpha[0])/(self.Omega[1]*self.Omega[2] - self.oldomega[1]*self.oldomega[2]))
+        #print "I factor apha " + str((self.Alpha[0] - oldalpha[0])/(self.Omega[1]*self.Omega[2] - self.oldomega[1]*self.oldomega[2]))
         #print self.Alpha
         #print "alpha second term(true) " + str((self.I[2]-self.I[1])*self.Omega[1]*self.Omega[2]/self.I[0])
     
@@ -255,10 +256,10 @@ class Body(object):
     
     def computeAcceleration(self):
         #rotate thrust
-        print "Thrust before rotation " + self.Thrust
-        print "Rotation " + self.Quat
+        #print "Thrust before rotation " + self.Thrust
+        #print "Rotation " + self.Quat
         T = self.Thrust.rotate(self.Quat)
-        print "Thrust " + T
+        #print "Thrust " + T
         self.Acceleration = T/self.Mass
         self.Acceleration = Params.Gravity+T*(1/self.Mass)+self.Friction
     
@@ -271,21 +272,18 @@ class Body(object):
     def calibrate(self,dt):
         self.UseController = False
         
-        for i in range(0,2):
+        for i in range(0,100):
             #set motor power
             #self.m1.setMeasure(40+i*5, dt)
             self.CtrlInput = [3+i*2, 0, 0, 0]
             #get point
-            self.nextStep(dt)
-            self.cali.estimateI(0, self.Alpha, self.Omega)
-            for i in range(0, int(10/dt)):
+            for j in range(0, int(0.1/dt)):
                 self.nextStep(dt)
-            self.cali.estimateI(0, self.Alpha, self.Omega)
-            self.nextStep(dt)
-            #self.cali.newOmega(0,self.Omega)
-            self.nextStep(dt)
-            #set point
-            self.cali.newPoint(0, 3+i*2, self.Omega, self.Alpha)
+            self.cali.newPoint(3, self.Omega, self.Alpha,0)
+            for j in range(0, int(0.1/dt)):
+                self.nextStep(dt)
+            self.cali.newPoint(3, self.Omega, self.Alpha,0)
+            self.cali.clearPoints()
         
         
         print self.cali.fI
@@ -312,5 +310,5 @@ if __name__ == "__main__":
     b.setModel(I, K_d, Params.L, Params.Mass)
     b.setParameters(Params.TorqueIsSet, Params.MaxTorque, True)
     b.setMotorConstants(Rho, K_v, K_t, K_tau, I_M, A_swept, A_xsec, Radius, C_D)
-    b.calibrate(0.01)
+    b.calibrate(0.001)
     sys.exit(0);
--- Simulation/DroneSimulator/Calibrator.py
+++ Simulation/DroneSimulator/Calibrator.py
@@ -5,64 +5,59 @@ class Calibrator:
     '''
     classdocs
     '''
-    fI = Matrix()
-    R = [Vector(),Vector(),Vector(),Vector()]
-
-    fP = [[]]*4
-    fOmega = [[]]*4
-    fAlpha = [[]]*4
+    fI = Vector()
+    R = [None,None,None,None]
     
-    Iomega = Vector()
-    Ialpha = Vector()
+    fP = []
+    Iomega = []
+    Ialpha = []
+    number = 0
 
     def __init__(self):
         '''
         Constructor
         '''
-    def newOmega(self,motor, omega):
-        self.Iomega.append(omega)
     
-    def newPoint(self, motor, p, omega, alpha):
-        self.fP[motor].append(p)
-        self.fOmega[motor].append(omega)
-        self.fAlpha[motor].append(alpha)
+    def newPoint(self, p, omega, alpha,motor):
+        self.fP.append(p)
+        self.Iomega.append(omega)
+        self.Ialpha.append(alpha)
         
         return self.calibrateIndividual(motor)
-    
-    def estimateI(self,motor,alpha,omega):
-        #This works if the time difference between both measurement is big enough and time step small enough
-        print "Ialpha " + str(self.Ialpha)
-        print "alpha " + str(alpha)
-        print "IOmega " + str(self.Iomega)
-        print "omega " + str(omega)
-        
-        I = (alpha[0]-self.Ialpha[0])/(omega[1]*omega[2]-self.Iomega[1]*self.Iomega[2])
-        print "estimated I " + str(I)
-        self.Ialpha = alpha
-        self.Iomega = omega
-        
-    def calibrate(self):
-        return True
 
     def calibrateIndividual(self, motor):
-        if len(self.fP[motor])<2:
+        if len(self.fP)<2:
             return False
         
-        fracsq = pow(self.fP[motor][0]/self.fP[motor][1],2)
-        omegaprod1 = self.fOmega[motor][0][1]*self.fOmega[motor][0][2]
-        omegaprod2 = self.fOmega[motor][1][1]*self.fOmega[motor][1][2]
-        I = (self.fAlpha[motor][0][0]*fracsq-self.fAlpha[motor][1][0])/(omegaprod2-omegaprod1*fracsq)
+        #This works if the time difference between both measurement is big enough and time step small enough
+        I1 = (self.Ialpha[1][0]-self.Ialpha[0][0])/(self.Iomega[1][1]*self.Iomega[1][2]-self.Iomega[0][1]*self.Iomega[0][2])
+        I2 = (self.Ialpha[1][1]-self.Ialpha[0][1])/(self.Iomega[1][0]*self.Iomega[1][2]-self.Iomega[0][0]*self.Iomega[0][2])
+        I3 = (self.Ialpha[1][2]-self.Ialpha[0][2])/(self.Iomega[1][0]*self.Iomega[1][1]-self.Iomega[0][0]*self.Iomega[0][1])
+        print "Estimated I1 " + str(I1)
+        #print "Estimated I2 " + str(I2)
+        #print "Estimated I3 " + str(I3)
+        if self.fI[0] == 0:
+            self.fI[0] = I1
+        else:
+            self.fI[0] = self.fI[0]+I1
+        self.number += 1
         
-        
-        print "Estimated I " + str(I)
-        I = 0.887005649718
+        print "I moyenne " + str(self.fI[0]/self.number)
+        #I1 = 0.887005649718
         #This works if I is well known and simulation step is small enough so that the difference between oldomega and omega is small
-        Rx = (self.fAlpha[motor][0][0] + omegaprod1*I)/pow(self.fP[motor][0],2)
-        print "Estimated Rx " + str(Rx)
+        Rx = (self.Ialpha[0][0] - self.Iomega[0][1]*self.Iomega[0][2]*I1)/pow(self.fP[0],2)
+        Ry = (self.Ialpha[0][1] + self.Iomega[0][0]*self.Iomega[0][2]*I2)/pow(self.fP[0],2)
+        Rz = (self.Ialpha[0][2] - self.Iomega[0][0]*self.Iomega[0][1]*I3)/pow(self.fP[0],2)
+        #print "Estimated Rx " + str(Rx)
+        #print "Estimated Ry " + str(Ry)
+        #print "Estimated Ry " + str(Rz)
+        
+        self.R[motor] = Vector([Rx,Ry,Rz])
+        
         return True
         
     def clearPoints(self):
-        self.fP = [[]]*4
-        self.fOmega = [[]]*4
-        self.fAlpha = [[]]*4
+        self.Ialpha = []
+        self.Iomega = []
+        self.fP = []
     
